d13ab55fcb9c4fa9e2b262ec835afc24d9925e02
Suparna <suparnaprasad.in@gmail.com>
2022-11-12 14:38:58 +0530

    New layout with google fonts

--- lib/WeatherInfoPage.dart
+++ lib/WeatherInfoPage.dart
@@ -1,13 +1,109 @@
-class SecondPage extends StatefulWidget {
-  const SecondPage({Key? key}) : super(key: key);
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:google_fonts/google_fonts.dart';
+
+// HAVE TO PASS VALUE OF USER CITY TO THIS PAGE.
+class WeatherInfoPage extends StatefulWidget {
+  String userCity;
+  WeatherInfoPage({required this.userCity});
+
 
   @override
-  State<SecondPage> createState() => _SecondPageState();
+  State<WeatherInfoPage> createState() => _WeatherInfoPageState();
 }
 
-class _SecondPageState extends State<SecondPage> {
+class _WeatherInfoPageState extends State<WeatherInfoPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
-  }
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: FutureBuilder(                        // future Builder is called as soon as the program begins. to prevent that, variable called isPressed, only when button pressed, it'll run future builder
+          future : apicall(widget.userCity),
+          builder: (context,snapshot){
+            if (snapshot.hasData){
+              return Container(
+                child: Stack(
+                  children: [
+                  Image.asset('.idea/images/pleasantBckg.webp',         // first thing in stack is this pic made to fill bckg size.
+                  fit: BoxFit.cover,
+                  height: double.infinity,
+                  width: double.infinity,),
+                Container(
+                  decoration: BoxDecoration(color: Colors.black38),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                      Container(
+                      child: Column(                                               // second in stack is this column which contains the text
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(height: 120),            // this is giving space between the city name and the top of the screen.
+                            Text('${widget.userCity}',
+                                  style: GoogleFonts.lato(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 35,
+                                  color: Colors.white)
+                              ,),
+                            Text('date and time', style: GoogleFonts.lato(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 25,
+                                color: Colors.white)
+                            ),
+                        Column(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.symmetric(vertical: 40),                 // WHAT IS MARGIN FOR A CONTAINER
+                                child: Column(
+                                  children: [
+                                    Text(snapshot.data['temperature'].toString() + " °C",
+                                    style: GoogleFonts.lato(fontSize: 100, color: Colors.white),),
+                                    Text("Humidity : " + snapshot.data['humidity'].toString(),
+                                    style: GoogleFonts.lato(fontSize: 30, color: Colors.white),)
+                                  ],
+                                ),
+                              ),
+
+
+              ]),
+              ]),
+                      )]
+            )
+            )
+            ]),
+            );
+
+
+            }
+            else{
+              return CircularProgressIndicator();
+            }
+          }
+    ),
+    );
+    }
 }
+// should pass the city that the user has entered in this http query.
+// reason why we put async is because this function, apicall() is retrieving data from the web, which might take some time,
+// thus making it asynchronous. So, it's response will also have a waiting time / await keyword.
+Future apicall(String cityVal) async{
+  final url = Uri.parse("https://api.openweathermap.org/data/2.5/weather?q="+cityVal+"&appid=faf469c4a3c46c7bf539017ab3325e0c");
+  final response = await http.get(url);         // final means the variable response does not change during the runtime of the code(during a particular request)
+  print (response.body);      // this body is of type string
+  final json = jsonDecode(response.body);
+  print (json["weather"][0]['description']);
+  final output = {
+    'description' : json["weather"][0]['description'],
+    'temperature' : ((json["main"]['temp']) - 273.15).round(),    // converting from kelvin to celsius.
+    'humidity': json['main']['humidity']
+  };
+  return output;
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:untitled/WeatherInfoPage.dart';
+
+// SHOULD FUTURE BUILDER FUNCTION BE PUT IN THE NEXT PAGE OR IS IT EASIER TO PASS VALUES BETWEEN SCREENS??
+
 
 void main() {
   runApp(const MyApp());
@@ -29,25 +30,22 @@ class _MyHomePageState extends State<MyHomePage> {
   final _textController = TextEditingController();      // textController is the widget that will have access to input in TextField
   String userCity = '';
   bool isPressed = false;
+  String bckgImg = '.idea/images/mountains_bkg.jpg';
+  //double temp = snapshot.data['temperature'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-          title: Center(
-            child: Text("Weather App",
-                style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white
-                )),
-          )
+          backgroundColor: Colors.transparent,
+          elevation: 0,
 
       ),
       body : Container(
         decoration : BoxDecoration(
           image : DecorationImage(
-            image : AssetImage('.idea/images/mountains_bkg.jpg'),          // always do right click of the image and copy content root. s
+            image : AssetImage(bckgImg),          // always do right click of the image and copy content root. s
             fit: BoxFit.cover,
           )
         ),
@@ -86,47 +84,67 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
               ElevatedButton(onPressed: (){
-                setState(() {
-                  userCity = _textController.text;
-                  isPressed = !isPressed;
-                });
+                Navigator.of(context).push(MaterialPageRoute(builder: (context)=>WeatherInfoPage(userCity: _textController.text)));
               },
+
+                // setState(() {
+                //   userCity = _textController.text;
+                //   isPressed = !isPressed;
+                // });
+
                   child: Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Text("Submit"),
                   )),
-              isPressed?FutureBuilder(                        // future Builder is called as soon as the program begins. to prevent that, variable called isPressed, only when button pressed, it'll run future builder
-                  future : apicall(userCity),
-                  builder: (context,snapshot){
-                    if (snapshot.hasData){
-                      return Column(
-                          children : [
-                            const Padding(
-                              padding: EdgeInsets.all(5.0),
-                              child: SizedBox(                    // to set dimensions of the TextField
-                                width : 300,
-                                height : 50,
-                                // child: TextField(
-                                //   decoration: InputDecoration(
-                                //     hintText: "Enter the city",
-                                //     border: OutlineInputBorder()
-                                //   ),
-                                // ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(12.0),
-                              child: Text(snapshot.data['description'].toString()),
-                            ),
-                            Text(snapshot.data['temperature'].toString() + " °C")
-                          ]
-                      );
 
-                    }
-                    else{
-                      return CircularProgressIndicator();
-                    }
-                  }):Text("")
+              // isPressed?FutureBuilder(                        // future Builder is called as soon as the program begins. to prevent that, variable called isPressed, only when button pressed, it'll run future builder
+              //     future : apicall(userCity),
+              //     builder: (context,snapshot){
+              //       if (snapshot.hasData){
+              //         double temp = snapshot.data['temperature'];
+              //         if (temp >= 25){
+              //           bckgImg = '.idea/images/desertBckg.jpg';
+              //           String weather = 'That\'s scorching!';
+              //         }
+              //         // else if ((25 <= temp) & (temp <= 30)){
+              //         //   bckgImg = '.idea/images/pleasantBckg.webp';
+              //         //   String weather = 'The perfect time for a stroll!';
+              //         // }
+              //         // else if (temp < 25) {
+              //         //   bckgImg = '.idea/images/chillyBckg.jpg';
+              //         //   String weather = 'That\'s chilly!';
+              //         // }
+              //         // else {
+              //         //   bckgImg = '.idea/images/mountains_bkg.jpg';
+              //         // }
+              //         return Column(
+              //             children : [
+              //               const Padding(
+              //                 padding: EdgeInsets.all(5.0),
+              //                 child: SizedBox(                    // to set dimensions of the TextField
+              //                   width : 300,
+              //                   height : 50,
+              //                   // child: TextField(
+              //                   //   decoration: InputDecoration(
+              //                   //     hintText: "Enter the city",
+              //                   //     border: OutlineInputBorder()
+              //                   //   ),
+              //                   // ),
+              //                 ),
+              //               ),
+              //               Padding(
+              //                 padding: const EdgeInsets.all(12.0),
+              //                 child: Text(snapshot.data['description'].toString()),
+              //               ),
+              //               Text(snapshot.data['temperature'].toString())
+              //             ]
+              //         );
+              //
+              //       }
+              //       else{
+              //         return CircularProgressIndicator();
+              //       }
+              //     }):Text("")
             ],
           ),
 
@@ -137,25 +155,26 @@ class _MyHomePageState extends State<MyHomePage> {
 // should pass the city that the user has entered in this http query.
 // reason why we put async is because this function, apicall() is retrieving data from the web, which might take some time,
 // thus making it asynchronous. So, it's response will also have a waiting time / await keyword.
-Future apicall(String cityVal) async{
-  // final url = Uri(host : "https://api.openweathermap.org",
-  //     path : "/data/2.5/weather",
-  //     queryParameters : {
-  //       //'q' : '${userCity}',
-  //       'q' : cityVal,
-  //       'appid' : "faf469c4a3c46c7bf539017ab3325e0c"
-  //     });
-  // print(url);
-  // print("hI");
-  final url = Uri.parse("https://api.openweathermap.org/data/2.5/weather?q="+cityVal+"&appid=faf469c4a3c46c7bf539017ab3325e0c");
-  final response = await http.get(url);         // final means the variable response does not change during the runtime of the code(during a particular request)
-  print (response.body);      // this body is of type string
-  final json = jsonDecode(response.body);
-  print (json["weather"][0]['description']);
-  final output = {
-    'description' : json["weather"][0]['description'],
-    'temperature' : ((json["main"]['temp']) - 273.15).round()         // converting from kelvin to celsius.
-  };
-  return output;
-  }
+// Future apicall(String cityVal) async{
+//   // final url = Uri(host : "https://api.openweathermap.org",
+//   //     path : "/data/2.5/weather",
+//   //     queryParameters : {
+//   //       //'q' : '${userCity}',
+//   //       'q' : cityVal,
+//   //       'appid' : "faf469c4a3c46c7bf539017ab3325e0c"
+//   //     });
+//   // print(url);
+//   // print("hI");
+//   final url = Uri.parse("https://api.openweathermap.org/data/2.5/weather?q="+cityVal+"&appid=faf469c4a3c46c7bf539017ab3325e0c");
+//   final response = await http.get(url);         // final means the variable response does not change during the runtime of the code(during a particular request)
+//   print (response.body);      // this body is of type string
+//   final json = jsonDecode(response.body);
+//   print (json["weather"][0]['description']);
+//   final output = {
+//     'description' : json["weather"][0]['description'],
+//     'temperature' : ((json["main"]['temp']) - 273.15).round()         // converting from kelvin to celsius.
+//   };
+//   print (((json["main"]['temp']) - 273.15).round());
+//   return output;
+//   }
 
